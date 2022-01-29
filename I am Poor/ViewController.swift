9220119ee78bfa260d2939f7fbc2d76545b9fee9
Leonardo Santos Severino <leonardo.osantos@mercadolivre.com>
2022-01-28 21:45:02 -0300

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Leonardo Santos Severino on 28/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
